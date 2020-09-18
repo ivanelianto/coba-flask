edd4cc51d5f2daaaa0717e93fd6966c1db2a8ae3
Ivan Elianto <purenaturalism@gmail.com>
2020-09-18 21:20:37 +0700

    feat(route): add users route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from app.controller import UserController
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return 'Hello World'
+
+@app.route('/users')
+def users():
+    return UserController.index()
