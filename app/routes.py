cd5597a280d9060d782b634a34d01d504af99251
Ivan Elianto <purenaturalism@gmail.com>
2020-09-22 20:01:21 +0700

    feat(user): add store route

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
 from app import app
 from app.controller import UserController
+from flask import request
 
 @app.route('/')
 @app.route('/index')
 def index():
     return 'Hello World'
 
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST'])
 def users():
-    return UserController.index()
+    if request.method == 'GET':
+        return UserController.index()
+    elif request.method == 'POST':
+        return UserController.store()
 
 @app.route('/users/<id>')
 def userDetail(id):
